90694b0de9a354332d1176644807cd0cdb9b4213
Michael Edelman <metalman@amazon.com>
2021-09-03 22:37:35 -0400

    work on lexer functionality

--- src/interpreter/mod.rs
+++ src/interpreter/mod.rs
@@ -1,19 +1,44 @@
 #![allow(unused_imports)]
+use colored::*;
 use std::fs::File;
 
-pub struct Interpreter {}
+pub struct Interpreter {
+    is_interactive: bool,
+    had_error: bool,
+}
 
 impl Interpreter {
     pub fn new() -> Interpreter {
-        Interpreter {}
+        Interpreter {
+            is_interactive: false,
+            had_error: false,
+        }
     }
 
-    pub fn run(&mut self) {}
+    pub fn run(&mut self) {
+        if self.had_error && !self.is_interactive {
+            panic!("Had Error");
+        }
+    }
 
     pub fn run_file(&mut self, _: &str) {
         // let mut file = match File::open(path) {
         //     Err(why) => panic!("Couldn't open {}: {}", path, why),
         //     Ok(file) => file,
         // };
+    }
+
+    pub fn error(line: i16, message: &str) {
+        Interpreter::report(line, " ", message);
+    }
+
+    fn report(line: i16, loc: &str, message: &str) {
+        println!(
+            "[line {}] {} {}: {}",
+            line,
+            "Error".red().bold(),
+            loc,
+            message
+        );
     }
 }
--- src/lexer/mod.rs
+++ src/lexer/mod.rs
@@ -1,7 +1,46 @@
-pub struct Lexer {}
+pub mod token;
+use token::{Token, TokenType};
+
+pub struct Lexer {
+    source: String,
+    tokens: Vec<Token>,
+    start: i16,
+    current: i16,
+    line: i16,
+}
 
 impl Lexer {
-    pub fn new() -> Lexer {
-        Lexer {}
+    pub fn new(source: String) -> Lexer {
+        Lexer {
+            source: source,
+            tokens: vec![],
+            start: 0,
+            current: 0,
+            line: 0,
+        }
     }
+
+    fn scan_tokens(&mut self) {
+        while !self.is_at_end() {
+            self.start = self.current;
+        }
+    }
+
+    fn scan_token(&mut self) {}
+
+    fn is_at_end(&mut self) -> bool {
+        return self.current >= self.source.len() as i16;
+    }
+
+    fn advance(&mut self) -> char {
+        return self.source.chars().nth(self.current as usize).unwrap();
+    }
+
+    fn add_token(&mut self, t: TokenType) {
+        // Slice source to add to token based off start and current
+        let token = Token::new(t, self.line, "".to_string());
+        self.tokens.push(token)
+    }
+
+    // Need to add overloaded functionality in scanner class in book
 }
